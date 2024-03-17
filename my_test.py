dfffe742804f15fe2c09530df603fdf6e9603870
hopeof-Greatmind <58543696+hopeof-Greatmind@users.noreply.github.com>
2024-03-17 15:12:41 +0900

    Create my_test.py
    Just test on new file creation.

---
+++ my_test.py
@@ -0,0 +1,18 @@
+# Inference for ONNX model
+
+import cv2
+cuda = True
+w = "yolov7-tiny.onnx"
+#img = cv2.imread('horses.jpg')  # image-based execute!
+
+import time
+import requests
+import random
+import numpy as np
+import onnxruntime as ort
+from PIL import Image
+from pathlib import Path
+from collections import OrderedDict,namedtuple
+
+providers = ['AzureExecutionProvider', 'CPUExecutionProvider'] if cuda else ['CPUExecutionProvider']
+session = ort.InferenceSession(w, providers=providers)
