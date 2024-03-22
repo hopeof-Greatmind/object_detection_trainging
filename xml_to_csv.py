e72a1e00a31f7c49eed814ad4a70a17f50da69f9
hopeof-Greatmind <58543696+hopeof-Greatmind@users.noreply.github.com>
2024-03-22 18:11:21 +0900

    This is my first push!

--- xml_to_csv.py
+++ xml_to_csv.py
@@ -6,6 +6,7 @@ python xml_to_csv.py -i [PATH_TO_IMAGES_FOLDER]/train -o [PATH_TO_ANNOTATIONS_FO
 # Create test data:
 python xml_to_csv.py -i [PATH_TO_IMAGES_FOLDER]/test -o [PATH_TO_ANNOTATIONS_FOLDER]/test_labels.csv
 """
+#modifed by Prof. Kim 2024. 03. 22
 
 import os
 import glob
